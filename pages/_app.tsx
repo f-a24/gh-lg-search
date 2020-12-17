74aa6e1aa5b7bdedcddae7517a0454e6f20c7293
Atsushi <niconyan713@gmail.com>
2020-12-17 23:51:35 +0900

    changed design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import App from 'next/app';
 import { createGlobalStyle } from 'styled-components';
 import { Reset } from 'styled-reset';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import pink from '@material-ui/core/colors/pink';
 
 const GlobalStyle = createGlobalStyle`
   html, body {
     height: 100%;
-    background-color: #fff;
+    background-color: #000;
+    background-image: radial-gradient(at center bottom, rgba(230, 30, 99, 0.73) 0%, rgba(29, 38, 53, 0) 69%);
+    color: #c9d1d9;
   }
   #__next {
     height: 100%;
@@ -15,6 +19,15 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: pink[500]
+    }
+  }
+});
+
 export default class extends App {
   render() {
     const { Component, pageProps } = this.props;
@@ -22,7 +35,9 @@ export default class extends App {
       <>
         <Reset />
         <GlobalStyle />
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </>
     );
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { safeLoad } from 'js-yaml';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
+import { Cell, Pie, PieChart, Tooltip } from 'recharts';
 
 type PropsType = {
   allLang: {
@@ -20,28 +20,40 @@ type LanguageType = {
   color: string;
 }[];
 
+type UserDataType = {
+  avatar: string;
+  bio: string;
+  login: string;
+  name: string;
+  lang: LanguageType;
+};
+
 const App: NextPage<PropsType> = ({ allLang }) => {
   const [userName, setUserName] = React.useState('');
-  const [langs, setLangs] = React.useState<LanguageType>([]);
+  const [userData, setUserData] = React.useState<UserDataType>({
+    avatar: '',
+    bio: '',
+    login: '',
+    name: '',
+    lang: []
+  });
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserName(e.target.value);
   };
   const clickHandler = async () => {
     const langsArray: string[] = [];
-    let page = 1;
-    const userUrl = `https://api.github.com/search/users?q=${encodeURI(
-      userName
-    )}`;
+    const userUrl = `https://api.github.com/users/${encodeURI(userName)}`;
     const userRes = await fetch(userUrl);
-    const userData = await userRes.json();
-    console.log(userData);
-    while (true) {
+    const { avatar_url, bio, login, name, public_repos } = await userRes.json();
+    const reposPage = public_repos / 100;
+
+    for (let i = 0; i <= reposPage; i++) {
       const reposUrl = `https://api.github.com/users/${encodeURI(
         userName
-      )}/repos?per_page=100&page=${page}`;
+      )}/repos?per_page=100&page=${i + 1}`;
       const reposRes = await fetch(reposUrl);
       const reposData = await reposRes.json();
-      if (reposData.length === 0) break;
+
       langsArray.push(
         ...reposData
           .filter(
@@ -50,60 +62,116 @@ const App: NextPage<PropsType> = ({ allLang }) => {
           )
           .map(({ language }) => language)
       );
-      page++;
     }
     const langNames = [...new Set(langsArray)];
-    setLangs(
-      langNames.map(l => ({
+    setUserData({
+      avatar: avatar_url,
+      bio,
+      login,
+      name,
+      lang: langNames.map(l => ({
         name: l,
         value: langsArray.filter(lang => lang === l).length,
         color: allLang.find(({ lang }) => lang === l).color
       }))
-    );
+    });
   };
   return (
-    <StyledArea>
-      <h1>Language percentage in Github repository</h1>
-      <TextField
-        label="User Name"
-        value={userName}
-        onChange={inputHandler}
-        fullWidth
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={clickHandler}
-        fullWidth
-      >
-        Search
-      </Button>
-      <ResponsiveContainer width="100%" height="80%">
-        <PieChart>
-          <Pie dataKey="value" data={langs} label={({ name }) => name}>
-            {langs.map(({ color }) => (
-              <Cell key={`${Math.random()}`} fill={color} />
-            ))}
-          </Pie>
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
-    </StyledArea>
+    <>
+      <InputBlock>
+        <h1>Language percentage in Github repository</h1>
+        <TextField
+          label="User Name"
+          value={userName}
+          onChange={inputHandler}
+          variant="outlined"
+          fullWidth
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={clickHandler}
+          fullWidth
+        >
+          Search
+        </Button>
+      </InputBlock>
+      {!!userData.login && (
+        <DataBlock>
+          <UserBlock>
+            <img src={userData.avatar} alt={userData.login} />
+            <p>{userData.name}</p>
+            <p>{userData.login}</p>
+            <p>{userData.bio}</p>
+          </UserBlock>
+          <ChartBlock>
+            <PieChart width={600} height={600}>
+              <Pie
+                dataKey="value"
+                data={userData.lang}
+                label={({ name }) => name}
+              >
+                {userData.lang.map(({ color }) => (
+                  <Cell key={`${Math.random()}`} fill={color} />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </ChartBlock>
+        </DataBlock>
+      )}
+    </>
   );
 };
 
-const StyledArea = styled.div`
-  width: 100%;
-  height: 100%;
+const InputBlock = styled.div`
   padding: 16px;
+  background-color: #0d1117;
   > h1 {
     font-size: 32px;
-    font-weight: bold;
     margin-bottom: 16px;
   }
   > button {
     margin-top: 16px;
   }
+`;
+
+const DataBlock = styled.div`
+  display: flex;
+`;
+
+const UserBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: flex-start;
+  padding: 16px;
+  margin: 16px;
+  max-width: 300px;
+  background-color: #0d1117;
+  > img {
+    width: 260px;
+    height: 260px;
+    border-radius: 50%;
+    margin-bottom: 16px;
+  }
+  > p:nth-of-type(1) {
+    font-size: 26px;
+    line-height: 1.25;
+  }
+  > p:nth-of-type(2) {
+    font-size: 20px;
+    font-weight: 300;
+    line-height: 24px;
+    margin-bottom: 16px;
+  }
+`;
+
+const ChartBlock = styled.div`
+  display: flex;
+  flex: 1;
+  padding: 16px;
+  margin: 16px;
+  background-color: #0d1117;
 `;
 
 export async function getStaticProps() {
